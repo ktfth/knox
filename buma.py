d496cdc2f98f2bfe7b05dd1b9bd68b34cd513a6d
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-07-31 05:24:11 -0300

    Capabilitie based on the gradient learning

--- buma.py
+++ buma.py
@@ -1,9 +1,27 @@
+import argparse
 import gym as g
 import retro as r
+import numpy as np
 import random as radix
 import tensorflow as hagnar
 
 import warnings as ignite ; ignite.simplefilter('ignore')
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--mode', type=str, default='rgb',
+							  help='Rendering mode')
+parser.add_argument('--environment', type=str, default='MsPacman-v0',
+									 help='Environment name')
+parser.add_argument('--episodes', type=int, default=10,
+								  help='Seens episode')
+parser.add_argument('--timesteps', type=int, default=1000,
+								   help='Watchout series')
+
+parser.add_argument('--policy_construct_file_path', type=str, default='solid_buma.h5',
+													help='Constructing buma')
+parser.add_argument('--policy_builder_file_path', type=str, default='solid_buma.h5',
+												  help='Builder buma')
 
 class DQNAdapter(object):
 	def __init__(self, *args, **kwargs):
@@ -23,8 +41,24 @@ class DQNFlyweight(DQNAdapter):
 	def step(self, _action):
 		return self.agent.step(_action)
 
-class PolicyGradientBuilder(hagnar.keras.Model):
+class PolicyGradientComposite(hagnar.keras.Model):
 	pass
+
+class PolicyGradientBuilder(object):
+	def __init__(self, *args, **kwargs):
+		super(type(object)).__init__()
+
+	def load(self, *ars, **kws):
+		pass
+
+	def generate(self, *ars, **kws):
+		return ('sample' in kws and kws['sample']) or ars[0]
+
+	def learn(self, *ars, **kws):
+		return ('samples_act' in kws and kws['samples_act']) or ars[0]
+
+	def save(self, *ars, **kws):
+		pass
 
 class ReinforcementLearning(DQNFlyweight):
 	def __init__(self, *args, **kwargs):
@@ -37,13 +71,16 @@ class ReinforcementLearning(DQNFlyweight):
 		if 'dqn' in kwargs:
 			self.dqn = kwargs['dqn']
 
-	def steps_action(self, _act):
+	def steps_action(self, _act, n=4):
 		dqn = self.dqn
 		act = _act
-		return (dqn.step(act), dqn.step(act), dqn.step(act), dqn.step(act)), \
-	           (dqn.step(act), dqn.step(act), dqn.step(act), dqn.step(act)), \
-	           (dqn.step(act), dqn.step(act), dqn.step(act), dqn.step(act)), \
-	           (dqn.step(act), dqn.step(act), dqn.step(act), dqn.step(act))
+		return (dqn.step(act) for _ in np.arange(n)), \
+	           (dqn.step(act) for _ in np.arange(n)), \
+	           (dqn.step(act) for _ in np.arange(n)), \
+	           (dqn.step(act) for _ in np.arange(n))
+
+class ReinforcementLearningMemento(object):
+	pass
 
 class AgentProxy(ReinforcementLearning):
 	def __init__(self, *args, **kwargs):
@@ -70,7 +107,7 @@ class AgentProxy(ReinforcementLearning):
 			action = kws['action']
 		return self.environment.step(action)
 
-class EnvironmentHoisiting(object):
+class EnvironmentHoisiting(ReinforcementLearningMemento):
 	def __init__(self, *args, **kwargs):
 		super(EnvironmentHoisiting, self).__init__()
 
@@ -104,14 +141,22 @@ class EnvironmentHoisiting(object):
 	def net(self, _dqn):
 		return ReinforcementLearning(_dqn)
 
-def main(unusued_argv):
-	virtualization, vm, rl, dqn, net = EnvironmentHoisiting('Enduro-ram-v0', g.make).instance()
-	for e in range(10):
+def main(argv):
+	args = parser.parse_args(argv[1:])
+
+	virtualization, vm, rl, dqn, net = EnvironmentHoisiting(args.environment, g.make).instance()
+
+	policy_gradient = PolicyGradientBuilder()
+
+	policy_gradient.load(args.policy_construct_file_path)
+
+	for e in np.arange(args.episodes):
 	    s = vm.reset()
-	    for t in range(1000):
-	        vm.render(mode='rgb')
-	        act = rl.action_space_down_sample()
-	        obs, rew, don, inf = net.steps_action(act)
+	    for t in np.arange(args.timesteps):
+	        vm.render(mode=args.mode)
+	        act = policy_gradient.generate(rl.action_space_down_sample())
+	        obs, rew, don, inf = policy_gradient.learn(net.steps_action(act))
+	        policy_gradient.save(args.policy_builder_file_path)
 	    if don:
 	    	break
 	vm.close()
