dcb271abdf07fe5751ab1cef2cf12e46ea50409f
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-08-04 15:44:41 -0300

    Increased the tests for policy grandient composite

--- app_test.py
+++ app_test.py
@@ -2,15 +2,24 @@ import tensorflow as tf
 
 from app import DQNAdapter
 from app import DQNFlyweight
+from app import PolicyGradientComposite
 
 class AppDQNTestCase(tf.test.TestCase):
 	def testDQNAdapterTypeClass(self):
 		with self.test_session():
-			self.assertEqual(type(DQNAdapter()), DQNAdapter)
+			self.assertEqual(type(DQNAdapter()),
+				             DQNAdapter)
 
 	def testDQNFlyweightTypeClass(self):
 		with self.test_session():
-			self.assertEqual(type(DQNFlyweight()), DQNFlyweight)
+			self.assertEqual(type(DQNFlyweight()),
+						     DQNFlyweight)
+
+class AppPolicyGradientTestCase(tf.test.TestCase):
+	def testPolicyGradientCompositeTypeClass(self):
+		with self.test_session():
+			self.assertEqual(type(PolicyGradientComposite()),
+							 PolicyGradientComposite)
 
 if __name__ == '__main__':
 	tf.test.main()
