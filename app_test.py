8935cfb9122e9de79be7a3da5e44e407119706a1
Kaique da Silva <kaique.silva.dev@gmail.com>
2018-08-04 15:55:04 -0300

    Incread tests for props based on policy gradient h params

--- app_test.py
+++ app_test.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from app import DQNAdapter
 from app import DQNFlyweight
 from app import PolicyGradientComposite
+from app import policy_gradient_h_params
 
 class AppDQNTestCase(tf.test.TestCase):
 	def testDQNAdapterTypeClass(self):
@@ -20,6 +21,21 @@ class AppPolicyGradientTestCase(tf.test.TestCase):
 		with self.test_session():
 			self.assertEqual(type(PolicyGradientComposite()),
 							 PolicyGradientComposite)
+
+class AppPolicyGradientHParams(tf.test.TestCase):
+	def testLearningRateValue(self):
+		with self.test_session():
+			h = policy_gradient_h_params
+			expectation = h.learning_rate
+			expected = 10e-7
+			self.assertEqual(expectation, expected)
+
+	def testEpsilonValue(self):
+		with self.test_session():
+			h = policy_gradient_h_params
+			expectation = h.epsilon
+			expected = 10e-3
+			self.assertEqual(expectation, expected)
 
 if __name__ == '__main__':
 	tf.test.main()
